f75c445252b12fb098435c289c5fd43c904ccc44
naufalmuafadart <mnaufalmuafa.dart@gmail.com>
2024-10-21 13:55:50 +0700

    Use template app on list announcement and announcement detail page

--- resources/views/announcements/list.blade.php
+++ resources/views/announcements/list.blade.php
@@ -1,44 +1,33 @@
 {{-- resources/views/pengumuman/index.blade.php --}}
-@extends('templates.blogs')
+@extends('templates.app')
 
 @section('container')
-    <!-- preloade -->
-    <div class="preload preload-container">
-        <div class="preload-logo">
-            <div class="spinner"></div>
-        </div>
-    </div>
-    <!-- /preload -->
-    <div class="header is-fixed">
+    <div class="card-secton transfer-section">
         <div class="tf-container">
-            <div class="tf-statusbar d-flex justify-content-center align-items-center">
-                <a href="{{ url()->previous() }}" class="back-btn"> <i class="icon-left"></i> </a>
-                <h3>{{ $title}}</h3>
+            <div class="tf-balance-box">
+                @if ($filtered_announcements->isNotEmpty())
+                    @foreach ($filtered_announcements as $announcement)
+                        <div class="food-box">
+                            <a href="{{ url('/pengumuman/show/'.$announcement->id) }}">
+                                <div class="content">
+                                    <!-- Link to the detailed view of the announcement -->
+                                    <h4>
+                                        {{ $announcement->title }}
+                                    </h4>
+                                    <!-- You can add more fields from the announcement like date, created_by, etc. -->
+                                    <span>{{ $announcement->created_at->format('d M Y') }}</span>
+                                </div>
+                            </a>
+                        </div>
+                        <hr>
+                    @endforeach
+                @else
+                    <!-- Show a message if no announcements are found -->
+                    <p>No announcements available for you.</p>
+                @endif
             </div>
         </div>
     </div>
-    <div id="app-wrap" class="style1">
-        <div class="tf-container">
-            @if ($filtered_announcements->isNotEmpty())
-                @foreach ($filtered_announcements as $announcement)
-                    <div class="food-box">
-                        <a href="{{ url('/pengumuman/show/'.$announcement->id) }}">
-                            <div class="content">
-                                <!-- Link to the detailed view of the announcement -->
-                                <h4>
-                                    {{ $announcement->title }}
-                                </h4>
-                                <!-- You can add more fields from the announcement like date, created_by, etc. -->
-                                <span>{{ $announcement->created_at->format('d M Y') }}</span>
-                            </div>
-                        </a>
-                    </div>
-                    <hr>
-                @endforeach
-            @else
-                <!-- Show a message if no announcements are found -->
-                <p>No announcements available for you.</p>
-            @endif
-        </div>
-    </div>
+    <br>
+    <br>
 @endsection
