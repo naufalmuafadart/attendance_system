23a16c63033c3d465e76c5e7ac856e695d0bf208
naufalmuafadart <mnaufalmuafa.dart@gmail.com>
2024-10-26 08:06:28 +0700

    Remove jejak keberhasilan menu

--- resources/views/karyawan/euforia.blade.php
+++ resources/views/karyawan/euforia.blade.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <!-- Mobile Specific Metas -->
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, viewport-fit=cover">
-    <title>rewards</title>
+    <title>Informasi</title>
     <!-- Favicon and Touch Icons  -->
     <link rel="shortcut icon" href="{{ url('/myhr/images/logo.png') }}" />
     <link rel="apple-touch-icon-precomposed" href="{{ url('/myhr/images/logo.png') }}" />
@@ -23,12 +23,12 @@
 </head>
 
 <body>
-       <!-- preloade -->
-       <div class="preload preload-container">
+    <!-- preloade -->
+    <div class="preload preload-container">
         <div class="preload-logo">
-          <div class="spinner"></div>
+            <div class="spinner"></div>
         </div>
-      </div>
+    </div>
     <!-- /preload -->
     <div class="header is-fixed">
         <div class="tf-container">
@@ -40,265 +40,27 @@
     </div>
     <div id="app-wrap" class="style1">
         <div class="tf-container">
-            <div class="tf-tab">
-                <ul class="menu-tabs tabs-food">
-                    <li class="nav-tab active">Hari Bahagia</li>
-                    <li class="nav-tab">Jejak Keberhasilan</li>
-                </ul>
-                <div class="content-tab pt-tab-space mb-5">
-                    <div class="tab-gift-item">
-                        @foreach ($data_user as $user)
-                            @php
-                                $tgl_lahir = new DateTime($user->tgl_lahir);
-                            @endphp
-                            <div class="food-box">
-                                <div class="img-box">
-                                    @if($user->foto_karyawan == null)
-                                        <img src="{{ url('/assets/img/foto_default.jpg') }}"  alt="image">
-                                    @else
-                                        <img src="{{ url('/storage/'.$user->foto_karyawan) }}"  alt="image">
-                                    @endif
-                                    <span>&nbsp;</span>
-                                </div>
-                                <div class="content">
-                                    <h4><a href="{{ url('/pegawai/show/'.$user->id) }}">{{ $user->name }}</a></h4>
-                                    <span>{{ $tgl_lahir->format('d M Y') }}</span>
-                                </div>
-                            </div>
-                        @endforeach
-                    </div>
-                    <div class="tab-gift-item-2">
-                        <div class="tes-gift-box">
-                            <h3>Victuals</h3>
-                            <div class="swiper-container tes-gift-2 mt-5">
-                                <div class="swiper-wrapper">
-                                    <div class="swiper-slide">
-                                        <div class="box-gift-card large bg_gift-card-1">
-                                            <ul class="desc">
-                                               <li class="name"><a href="#" class="on_surface_color fw_7"> THE TEST KITCHEN</a> </li>
-                                               <li class="code success_color fw_6">R100</li>
-                                               <li class="counpon">COUPON</li>
-                                               <li>
-                                                    <i class="icon-noti"></i>
-                                                    Term of use
-                                               </li>
-                                            </ul>
-                                            <div class="img-gift">
-                                                <img src="{{ url('/myhr/images/rewards/gift-1.jpg') }}" alt="images">
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="swiper-slide">
-                                        <div class="box-gift-card large bg_gift-card-2">
-                                            <ul class="desc">
-                                               <li class="name"><a href="#" class="white_color fw_5">THE TEST KITCHEN</a></li>
-                                               <li class="code success_color fw_6">R100</li>
-                                               <li class="counpon white_color fw_4">COUPON</li>
-                                               <li class="white_color fw_4">
-                                                    <i class="icon-noti white_color"></i>
-                                                    Term of use
-                                               </li>
-                                            </ul>
-                                            <div class="img-gift">
-                                                <img src="{{ url('/myhr/images/rewards/gift-2.jpg') }}" alt="images">
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="swiper-slide">
-                                        <div class="box-gift-card large bg_gift-card-1">
-                                            <ul class="desc">
-                                               <li class="name"><a href="#" class="on_surface_color fw_7"> THE TEST KITCHEN</a></li>
-                                               <li class="code success_color fw_6">R100</li>
-                                               <li class="counpon">COUPON</li>
-                                               <li>
-                                                    <i class="icon-noti"></i>
-                                                    Term of use
-                                               </li>
-                                            </ul>
-                                            <div class="img-gift">
-                                                <img src="{{ url('/myhr/images/rewards/gift-1.jpg') }}" alt="images">
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class= "mt-2 swiper-pagination dots-tes dots-food-tes"></div>
-                            </div>
+            <div class="tab-gift-item">
+                @foreach ($data_user as $user)
+                    @php
+                        $tgl_lahir = new DateTime($user->tgl_lahir);
+                    @endphp
+                    <div class="food-box">
+                        <div class="img-box">
+                            @if($user->foto_karyawan == null)
+                                <img src="{{ url('/assets/img/foto_default.jpg') }}"  alt="image">
+                            @else
+                                <img src="{{ url('/storage/'.$user->foto_karyawan) }}"  alt="image">
+                            @endif
+                            <span>&nbsp;</span>
                         </div>
-
-                        <div class="tes-gift-box mt-4">
-                            <h3>Kitchen</h3>
-                            <div class="swiper-container tes-gift-2 mt-5">
-                                <div class="swiper-wrapper">
-                                    <div class="swiper-slide">
-                                        <div class="box-gift-card large bg_gift-card-2">
-                                            <ul class="desc">
-                                                <li class="name"><a href="#" class="white_color fw_5">THE TEST KITCHEN</a></li>
-                                               <li class="code success_color fw_6">R100</li>
-                                               <li class="counpon white_color fw_4">COUPON</li>
-                                               <li class="white_color fw_4">
-                                                    <i class="icon-noti white_color"></i>
-                                                    Term of use
-                                               </li>
-                                            </ul>
-                                            <div class="img-gift">
-                                                <img src="{{ url('/myhr/images/rewards/gift-2.jpg') }}" alt="images">
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="swiper-slide">
-                                        <div class="box-gift-card large bg_gift-card-1">
-                                            <ul class="desc">
-                                               <li class="name"><a href="#" class="on_surface_color fw_7"> THE TEST KITCHEN</a></li>
-                                               <li class="code success_color fw_6">R100</li>
-                                               <li class="counpon">COUPON</li>
-                                               <li>
-                                                    <i class="icon-noti"></i>
-                                                    Term of use
-                                               </li>
-                                            </ul>
-                                            <div class="img-gift">
-                                                <img src="{{ url('/myhr/images/rewards/gift-1.jpg') }}" alt="images">
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="swiper-slide">
-                                        <div class="box-gift-card large bg_gift-card-2">
-                                            <ul class="desc">
-                                                <li class="name"><a href="#" class="white_color fw_5">THE TEST KITCHEN</a></li>
-                                               <li class="code success_color fw_6">R100</li>
-                                               <li class="counpon white_color fw_4">COUPON</li>
-                                               <li class="white_color fw_4">
-                                                    <i class="icon-noti white_color"></i>
-                                                    Term of use
-                                               </li>
-                                            </ul>
-                                            <div class="img-gift">
-                                                <img src="{{ url('/myhr/images/rewards/gift-2.jpg') }}" alt="images">
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class= "mt-2 swiper-pagination dots-tes dots-food-tes"></div>
-                            </div>
-                        </div>
-                        <div class="tes-gift-box mt-4">
-                            <h3>Restaurant</h3>
-                            <div class="swiper-container tes-gift-2 mt-5">
-                                <div class="swiper-wrapper">
-                                    <div class="swiper-slide">
-                                        <div class="box-gift-card large bg_gift-card-1">
-                                            <ul class="desc">
-                                                <li class="name"><a href="#" class="on_surface_color fw_7"> THE TEST KITCHEN</a></li>
-                                               <li class="code success_color fw_6">R100</li>
-                                               <li class="counpon">COUPON</li>
-                                               <li>
-                                                    <i class="icon-noti"></i>
-                                                    Term of use
-                                               </li>
-                                            </ul>
-                                            <div class="img-gift">
-                                                <img src="{{ url('/myhr/images/rewards/gift-1.jpg') }}" alt="images">
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="swiper-slide">
-                                        <div class="box-gift-card large bg_gift-card-1">
-                                            <ul class="desc">
-                                                <li class="name"><a href="#" class="on_surface_color fw_7">THE TEST KITCHEN</a> </li>
-                                               <li class="code success_color fw_6">R100</li>
-                                               <li class="counpon">COUPON</li>
-                                               <li>
-                                                    <i class="icon-noti"></i>
-                                                    Term of use
-                                               </li>
-                                            </ul>
-                                            <div class="img-gift">
-                                                <img src="{{ url('/myhr/images/rewards/gift-1.jpg') }}" alt="images">
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="swiper-slide">
-                                        <div class="box-gift-card large bg_gift-card-2">
-                                            <ul class="desc">
-                                                <li class="name"><a href="#" class="white_color fw_5">THE TEST KITCHEN</a></li>
-                                               <li class="code success_color fw_6">R100</li>
-                                               <li class="counpon white_color fw_4">COUPON</li>
-                                               <li class="white_color fw_4">
-                                                    <i class="icon-noti white_color"></i>
-                                                    Term of use
-                                               </li>
-                                            </ul>
-                                            <div class="img-gift">
-                                                <img src="{{ url('/myhr/images/rewards/gift-2.jpg') }}" alt="images">
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class= "mt-2 swiper-pagination dots-tes dots-food-tes"></div>
-                            </div>
-                        </div>
-                        <div class="tes-gift-box mt-4">
-                            <h3>Victuals</h3>
-                            <div class="swiper-container tes-gift-2 mt-5">
-                                <div class="swiper-wrapper">
-                                    <div class="swiper-slide">
-                                        <div class="box-gift-card large bg_gift-card-2">
-                                            <ul class="desc">
-                                               <li class="name"><a href="#" class="white_color fw_5">THE TEST KITCHEN</a></li>
-                                               <li class="code success_color fw_6">R100</li>
-                                               <li class="counpon white_color fw_4">COUPON</li>
-                                               <li class="white_color fw_4">
-                                                    <i class="icon-noti white_color"></i>
-                                                    Term of use
-                                               </li>
-                                            </ul>
-                                            <div class="img-gift">
-                                                <img src="{{ url('/myhr/images/rewards/gift-2.jpg') }}" alt="images">
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="swiper-slide">
-                                        <div class="box-gift-card large bg_gift-card-2">
-                                            <ul class="desc">
-                                                <li class="name"><a href="#" class="white_color fw_5">THE TEST KITCHEN</a></li>
-                                               <li class="code success_color fw_6">R100</li>
-                                               <li class="counpon white_color fw_4">COUPON</li>
-                                               <li class="white_color fw_4">
-                                                    <i class="icon-noti white_color"></i>
-                                                    Term of use
-                                               </li>
-                                            </ul>
-                                            <div class="img-gift">
-                                                <img src="{{ url('/myhr/images/rewards/gift-2.jpg') }}" alt="images">
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="swiper-slide">
-                                        <div class="box-gift-card large bg_gift-card-1">
-                                            <ul class="desc">
-                                                <li class="name"><a href="#" class="on_surface_color fw_7"></a> THE TEST KITCHEN</li>
-                                               <li class="code success_color fw_6">R100</li>
-                                               <li class="counpon">COUPON</li>
-                                               <li>
-                                                    <i class="icon-noti"></i>
-                                                    Term of use
-                                               </li>
-                                            </ul>
-                                            <div class="img-gift">
-                                                <img src="{{ url('/myhr/images/rewards/gift-1.jpg') }}" alt="images">
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class= "mt-2 swiper-pagination dots-tes dots-food-tes"></div>
-                            </div>
+                        <div class="content">
+                            <h4><a href="{{ url('/pegawai/show/'.$user->id) }}">{{ $user->name }}</a></h4>
+                            <span>{{ $tgl_lahir->format('d M Y') }}</span>
                         </div>
                     </div>
-
-                </div>
+                @endforeach
             </div>
-
-
         </div>
     </div>
 
