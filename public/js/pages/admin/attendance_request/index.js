fb25bed7274af8fd79f802440ba3a9d149c48ffe
naufalmuafadart <mnaufalmuafa.dart@gmail.com>
2024-10-30 13:30:21 +0700

    Show attendance image in modal

--- public/js/pages/admin/attendance_request/index.js
+++ public/js/pages/admin/attendance_request/index.js
@@ -93,19 +93,174 @@ const rowUser = {
     }
 }
 
+const detailModal = {
+    props: {
+        user_name: String,
+        date: String,
+        shift_name: String,
+        clock_in: String,
+        clock_out: String,
+        reason: String,
+        is_shown: Boolean,
+        status: String,
+        file: String,
+        index: Number,
+    },
+    data() {
+        return {
+            modalDisplay: 'none',
+            btn_reject_has_clicked: false,
+            imageURL: '',
+        };
+    },
+    async mounted() {
+        if (this.isFileIsAnImage(this.file)) {
+            await this.displayImage();
+        }
+    },
+    setup() {
+        const reasonModel = ref('');
+        return {
+            reasonModel,
+        };
+    },
+    watch: {
+        is_shown(newValue, oldValue) {
+            this.modalDisplay = newValue ? 'block' : 'none';
+            this. btn_reject_has_clicked = false;
+        },
+        async file(newValue, oldValue) {
+            if (this.isFileIsAnImage(newValue)) {
+                await this.displayImage();
+            }
+        }
+    },
+    template: `
+        <div id="largeModal" class="custom-modal-overlay" :style="{ 'display': modalDisplay }">
+            <div class="custom-modal-content">
+                <span class="custom-modal-close-button" @click="onClose">&times;</span>
+                <h2>Detail pengajuan absen</h2>
+                <table class="table table-bordered table-striped mb-2">
+                    <tbody>
+                    <tr>
+                        <td>Nama Karyawan</td>
+                        <td style="text-align: right;">{{ user_name }}</td>
+                    </tr>
+                    <tr>
+                        <td>Tanggal</td>
+                        <td style="text-align: right;">{{ date }}</td>
+                    </tr>
+                    <tr>
+                        <td>Shift</td>
+                        <td style="text-align: right;">{{ shift_name }}</td>
+                    </tr>
+                    <tr>
+                        <td>Clock In</td>
+                        <td style="text-align: right;">{{ clock_in == null ? '-' : clock_in }}</td>
+                    </tr>
+                    <tr>
+                        <td>Clock out</td>
+                        <td style="text-align: right;">{{ clock_out == null ? '-' : clock_out }}</td>
+                    </tr>
+                    <tr>
+                        <td>Alasan</td>
+                        <td style="text-align: right;">{{ reason }}</td>
+                    </tr>
+                    <tr>
+                        <td>File</td>
+                        <td style="text-align: right;" v-if="!isFileIsAnImage(file)">
+                            <button @click="downloadFile">Download</button>
+                        </td>
+                        <td v-else="" style="text-align: right"><img :src="imageURL" alt="prove image" style="max-width: 50%;"/></td>
+                    </tr>
+                    <tr>
+                        <td>Status</td>
+                        <td style="text-align: right;">{{ status }}</td>
+                    </tr>
+                    <tr v-if="status==='rejected'">
+                        <td>Alasan ditolak</td>
+                        <td style="text-align: right;">{{ reject_reason }}</td>
+                    </tr>
+                    </tbody>
+                </table>
+
+                <div class="d-flex justify-content-center" v-if="status === 'pending'">
+                    <button class="btn btn-info" @click="onApprove">
+                        <i data-feather="check" style="height: 1rem"></i>
+                        Terima
+                    </button>
+                    <div style="width: 10px;"></div>
+                    <button class="btn btn-danger" @click="onBtnRejectClicked">
+                        <i data-feather="x" style="height: 1rem"></i>
+                        Tolak
+                    </button>
+                </div>
+                <form v-if="status === 'pending' && btn_reject_has_clicked" @submit.prevent="onReject">
+                    <h3>Alasan Penolakan</h3>
+                    <div class="form-group">
+                        <input type="text" class="form-control" v-model="reasonModel">
+                    </div>
+                    <button class="btn btn-info">Submit Penolakan</button>
+                </form>
+            </div>
+        </div>
+    `,
+    methods: {
+        onBtnRejectClicked() {
+            this.btn_reject_has_clicked = true;
+        },
+        onApprove() {
+            this.$emit('on-approve');
+        },
+        onReject() {
+            this.$emit('on-reject', this.reasonModel);
+        },
+        onClose() {
+            this.$emit('close-modal');
+        },
+        isFileIsAnImage(fileName) {
+            // Define the common image extensions
+            const imageExtensions = ['jpg', 'jpeg', 'png', 'gif', 'bmp', 'webp', 'svg', 'tiff'];
+
+            // Get the file extension in lowercase
+            const extension = fileName.split('.').pop().toLowerCase();
+
+            // Check if the extension is in the list of image extensions
+            return imageExtensions.includes(extension);
+        },
+        async displayImage() {
+            const myHeaders = new Headers();
+            myHeaders.append("Content-Type", "application/json");
+
+            const raw = JSON.stringify({
+                "path": this.file
+            });
+
+            const requestOptions = {
+                method: "POST",
+                headers: myHeaders,
+                body: raw,
+                redirect: "follow"
+            };
+
+            const response = await fetch("/api/download", requestOptions);
+            if (response.ok) {
+                const blob = await response.blob();
+                this.imageURL = URL.createObjectURL(blob);
+            }
+        },
+        downloadFile() {
+            this.$emit('download-file', this.index);
+        },
+    }
+};
+
 const app = createApp({
     data() {
         return {
             'requests': [],
             isModalVisible: false,
             selected_request_index: -1,
-            btn_reject_has_clicked: false,
-        };
-    },
-    setup() {
-        const reasonModel = ref('');
-        return {
-            reasonModel,
         };
     },
     async mounted() {
@@ -118,11 +273,6 @@ const app = createApp({
             }
         }
     },
-    computed: {
-        modalDisplay() {
-            return this.isModalVisible ? 'block' : 'none';
-        },
-    },
     methods: {
         openModal(index) {
             this.selected_request_index = index;
@@ -130,51 +280,6 @@ const app = createApp({
         },
         closeModal() {
             this.isModalVisible = false;
-            this.btn_reject_has_clicked = false;
-        },
-        onBtnRejectClicked() {
-            this.btn_reject_has_clicked = true;
-        },
-        async onApprove() {
-            if (this.selected_request_index > -1) {
-                if (confirm('Apakah anda yakin akan menerima pengajuan absen ini?')) {
-                    const url = `/api/attendance_request/approve/${this.requests[this.selected_request_index].id}`;
-                    const response = await fetch(url, {
-                        method: 'POST',
-                    });
-                    const responseJSON = await response.json();
-                    if (response.status === 200) {
-                        window.location.reload();
-                    } else {
-                        alert(responseJSON.message);
-                    }
-                }
-            }
-        },
-        async onReject() {
-            if (this.selected_request_index > -1) {
-                if (confirm('Apakah anda yakin akan menolak pengajuan absen ini?')) {
-                    const url = `/api/attendance_request/reject/${this.requests[this.selected_request_index].id}`;
-                    const data = { 'reason': this.reasonModel };
-                    if (data['reason'] === '') {
-                        alert('Alasan penolakan tidak boleh kosong');
-                        return;
-                    }
-                    const response = await fetch(url, {
-                        method: 'POST',
-                        body: JSON.stringify(data),
-                        headers: {
-                            'Content-Type': 'application/json',
-                        },
-                    });
-                    const responseJSON = await response.json();
-                    if (response.status === 200) {
-                        window.location.reload();
-                    } else {
-                        alert(responseJSON.message);
-                    }
-                }
-            }
         },
         downloadFile(index) {
             if (index > -1) {
@@ -207,9 +312,51 @@ const app = createApp({
                     .catch((error) => console.error(error));
             }
         },
+        async onApprove() {
+            if (this.selected_request_index > -1) {
+                if (confirm('Apakah anda yakin akan menerima pengajuan absen ini?')) {
+                    const url = `/api/attendance_request/approve/${this.requests[this.selected_request_index].id}`;
+                    const response = await fetch(url, {
+                        method: 'POST',
+                    });
+                    const responseJSON = await response.json();
+                    if (response.status === 200) {
+                        window.location.reload();
+                    } else {
+                        alert(responseJSON.message);
+                    }
+                }
+            }
+        },
+        async onReject(reason) {
+            if (this.selected_request_index > -1) {
+                if (confirm('Apakah anda yakin akan menolak pengajuan absen ini?')) {
+                    const url = `/api/attendance_request/reject/${this.requests[this.selected_request_index].id}`;
+                    const data = { reason };
+                    if (data['reason'] === '') {
+                        alert('Alasan penolakan tidak boleh kosong');
+                        return;
+                    }
+                    const response = await fetch(url, {
+                        method: 'POST',
+                        body: JSON.stringify(data),
+                        headers: {
+                            'Content-Type': 'application/json',
+                        },
+                    });
+                    const responseJSON = await response.json();
+                    if (response.status === 200) {
+                        window.location.reload();
+                    } else {
+                        alert(responseJSON.message);
+                    }
+                }
+            }
+        },
     },
 });
 
 app.component('row-user', rowUser);
+app.component('detail-modal', detailModal);
 
 app.mount('#app');
--- resources/views/admin/attendance_request/index.blade.php
+++ resources/views/admin/attendance_request/index.blade.php
@@ -97,66 +97,20 @@
     </div>
 
     <!-- Modal Structure -->
-    <div id="largeModal" class="custom-modal-overlay" :style="{ 'display': modalDisplay }">
-        <div class="custom-modal-content" v-if="selected_request_index > -1">
-            <span class="custom-modal-close-button" @click="closeModal()">&times;</span>
-            <h2>Detail pengajuan absen</h2>
-            <table class="table table-bordered table-striped mb-2">
-                <tbody>
-                <tr>
-                    <td>Nama Karyawan</td>
-                    <td style="text-align: right;">@{{ requests[selected_request_index]['user_name'] }}</td>
-                </tr>
-                <tr>
-                    <td>Tanggal</td>
-                    <td style="text-align: right;">@{{ requests[selected_request_index]['date'] }}</td>
-                </tr>
-                <tr>
-                    <td>Shift</td>
-                    <td style="text-align: right;">@{{ requests[selected_request_index]['shift_name'] }}</td>
-                </tr>
-                <tr>
-                    <td>Clock In</td>
-                    <td style="text-align: right;">@{{ requests[selected_request_index]['clock_in'] == null ? '-' : requests[selected_request_index]['clock_in'] }}</td>
-                </tr>
-                <tr>
-                    <td>Clock out</td>
-                    <td style="text-align: right;">@{{ requests[selected_request_index]['clock_out'] == null ? '-' : requests[selected_request_index]['clock_out'] }}</td>
-                </tr>
-                <tr>
-                    <td>Alasan</td>
-                    <td style="text-align: right;">@{{ requests[selected_request_index]['reason'] }}</td>
-                </tr>
-                <tr>
-                    <td>File</td>
-                    <td style="text-align: right;"><button @click="downloadFile(selected_request_index)">Download</button></td>
-                </tr>
-                <tr>
-                    <td>Status</td>
-                    <td style="text-align: right;">@{{ requests[selected_request_index]['status'] }}</td>
-                </tr>
-                <tr v-if="requests[selected_request_index]['status']==='rejected'">
-                    <td>Alasan ditolak</td>
-                    <td style="text-align: right;">@{{ requests[selected_request_index]['reject_reason'] }}</td>
-                </tr>
-                </tbody>
-            </table>
-
-            <div class="d-flex justify-content-center" v-if="requests[selected_request_index]['status'] === 'pending'">
-                <button class="btn btn-info" @click="onApprove()"><i data-feather="check" style="height: 1rem"></i>Terima</button>
-                <div style="width: 10px;"></div>
-                <button class="btn btn-danger" @click="onBtnRejectClicked">
-                    <i data-feather="x" style="height: 1rem"></i>
-                    Tolak
-                </button>
-            </div>
-            <form v-if="requests[selected_request_index]['status'] === 'pending' && btn_reject_has_clicked" @submit.prevent="onReject">
-                <h3>Alasan Penolakan</h3>
-                <div class="form-group">
-                    <input type="text" class="form-control" v-model="reasonModel">
-                </div>
-                <button class="btn btn-info">Submit Penolakan</button>
-            </form>
-        </div>
-    </div>
+    <detail-modal
+            v-if="selected_request_index>-1"
+            @close-modal="closeModal"
+            @on-reject="onReject"
+            @on-approve="onApprove"
+            @download-file="downloadFile"
+            :user_name="requests[selected_request_index]['user_name']"
+            :date="requests[selected_request_index]['date']"
+            :shift_name="requests[selected_request_index]['shift_name']"
+            :clock_in="requests[selected_request_index]['clock_in']"
+            :clock_out="requests[selected_request_index]['clock_out']"
+            :reason="requests[selected_request_index]['reason']"
+            :status="requests[selected_request_index]['status']"
+            :file="requests[selected_request_index]['file']"
+            :is_shown="isModalVisible"
+            :index="selected_request_index"></detail-modal>
 @endsection
